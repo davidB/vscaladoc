81a8b226586ece809bba5c6c8c458c93bf759062
David Bernard <david.bernard.31@gmail.com>
2009-02-28 23:19:16 +0100

    restore the support for @codeAsDoc

--- src/main/scala/org/scala_tools/vscaladoc/ModelExtractor.scala
+++ src/main/scala/org/scala_tools/vscaladoc/ModelExtractor.scala
@@ -26,6 +26,43 @@ trait ModelExtractor extends OrigModelExtractor {
   /** a method with no parameters and return type != Unit */
   def isDefField(e: Symbol) = (e.isMethod && !e.isConstructor && !e.hasFlag(symtab.Flags.ACCESSOR) && (e.tpe.kind != "NoType"))
 
+  // override original code to support more tags
+  import java.util.regex.Pattern
+  // patterns for standard tags with 1 and 2 arguments
+  protected
+  val pat1 = Pattern.compile("[ \t]*@(author|deprecated|pre|return|see|since|todo|version|ex|note|codeAsDoc)[ \t]*(.*)")
+  protected
+  val pat2 = Pattern.compile("[ \t]*@(exception|param|throws)[ \t]+(\\p{Graph}*)[ \t]*(.*)")
+
+  override
+  protected def decodeComment(comment0: String): Comment = {
+    val comment = comment0 // .substring("/**".length, comment0.length - "*/".length)
+    val tok = new java.util.StringTokenizer(comment, LINE_SEPARATOR)
+    val buf = new StringBuilder
+    type AttrDescr = (String, String, StringBuilder)
+    val attributes = new collection.mutable.ListBuffer[AttrDescr]
+    var attr: AttrDescr = null
+    while (tok.hasMoreTokens) {
+      val s = tok.nextToken.replaceFirst("\\p{Space}?\\*", "")
+      val mat1 = pat1.matcher(s)
+      if (mat1.matches) {
+        attr = (mat1.group(1), null, new StringBuilder(mat1.group(2)))
+        //if (kind != CONSTRUCTOR)
+        attributes += attr
+      } else {
+        val mat2 = pat2.matcher(s)
+        if (mat2.matches) {
+          attr = (mat2.group(1), mat2.group(2), new StringBuilder(mat2.group(3)))
+          //if (kind != CLASS)
+          attributes += attr
+        } else if (attr ne null)
+          attr._3.append(s + LINE_SEPARATOR)
+        else
+          buf.append(s + LINE_SEPARATOR)
+      }
+    }
+    Comment(buf.toString, attributes.toList.map({x => Tag(x._1,x._2,x._3.toString)}))
+  }
   //isValOrVar(e) || isChildObject(e) || isDefField(e)
   //  ((e.isValue) && e.hasFlag(symtab.Flags.ACCESSOR)) || (e.isModule && !e.isRoot) || (e.isMethod && !e.isConstructor && !e.hasFlag(symtab.Flags.ACCESSOR) && (e.tpe.kind != "NoType"))
 //  val ExFields = new Category("ExField")(e => (e.isValue) && e.hasFlag(symtab.Flags.ACCESSOR)) {
